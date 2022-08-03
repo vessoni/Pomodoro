cd73736b5dd46028513d46d5ee723efad1f1c671
Alexandre Vessoni <vessonia@jbs.cam.ac.uk>
2022-08-03 12:34:39 +0100

    Structure

---
+++ src/components/Hello/index.tsx
@@ -0,0 +1,18 @@
+const Hello = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        textAlign: 'center',
+        paddingTop: '50px',
+      }}
+    >
+      Pomodoro
+    </div>
+  );
+};
+
+export default Hello;

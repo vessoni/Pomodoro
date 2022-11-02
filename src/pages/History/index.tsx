771a41bd9a6b34e93c450b96477483ba9c2927b2
Alexandre Vessoni <av.vessoni@gmail.com>
2022-11-02 16:25:01 +0000

    Listing history

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -9,8 +9,6 @@ export function History() {
     <HistoryContainer>
       <h1>My History</h1>
 
-      <pre>{JSON.stringify(cycles, null, 2)}</pre>
-
       <HistoryList>
         <table>
           <thead>
@@ -22,38 +20,22 @@ export function History() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Task</td>
-              <td>20 minutes</td>
-              <td>2 month ago</td>
-              <td>
-                <Status statusColor='green'>Done</Status>
-              </td>
-            </tr>
-            <tr>
-              <td>Task</td>
-              <td>20 minutes</td>
-              <td>2 month ago</td>
-              <td>
-                <Status statusColor='green'>Done</Status>
-              </td>
-            </tr>
-            <tr>
-              <td>Task</td>
-              <td>20 minutes</td>
-              <td>2 month ago</td>
-              <td>
-                <Status statusColor='yellow'>In progress</Status>
-              </td>
-            </tr>
-            <tr>
-              <td>Task</td>
-              <td>20 minutes</td>
-              <td>2 month ago</td>
-              <td>
-                <Status statusColor='red'>Interrupted</Status>
-              </td>
-            </tr>
+            {cycles.map((cycle) => {
+              return (
+                <tr key={cycle.id}>
+                  <td>{cycle.task}</td>
+                  <td>{cycle.minutesAmount} minutes</td>
+                  <td>{cycle.startDate.toISOString()}</td>
+                  <td>
+                    {cycle.finishedDate && <Status statusColor='green'>Done</Status>}
+                    {cycle.interruptedDate && <Status statusColor='red'>Interrupted</Status>}
+                    {!cycle.finishedDate && !cycle.interruptedDate && (
+                      <Status statusColor='yellow'>In progress</Status>
+                    )}
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </HistoryList>
